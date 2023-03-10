48feb65cf69e782aa7d02049025a91b351a33a00
Samuel <samuelluishm@gmail.com>
2023-03-10 16:57:38 +0100

    Front-end api simplificada

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -1,170 +1,123 @@
-const ENDPOINT = 'http://localhost'
+/* Async petition*/
+
+async function fetchServer(body, route) {
+  const ENDPOINT = 'http://localhost'
+
+  const args = {
+    method: 'POST',
+    headers: {
+      'Content-type': 'application/json',
+    },
+    body: body,
+  }
+
+  const response = await fetch(ENDPOINT + route, args)
+  if (!response.ok) throw new Error('Respuesta no OK')
+  return response.json()
+}
 
 /* Usuarios */
 
 export async function fetchUsuarios() {
-  const args = {
-    method: 'POST',
-    headers: {
-      'Content-type': 'application/json',
-    },
-  }
-  const response = await fetch(`${ENDPOINT}/api/usuarios.php`, args)
-  if (!response.ok) throw new Error('Respuesta no OK')
-  return response.json()
+  return fetchServer(null, '/api/usuarios.php')
 }
 
 export async function fetchUsuario(jwt, mail) {
-  const args = {
-    method: 'POST',
-    headers: {
-      'Content-type': 'application/json',
-    },
-    body: JSON.stringify({
+  return fetchServer(
+    JSON.stringify({
       jwt: jwt,
       mail: mail,
     }),
-  }
-  const response = await fetch(`${ENDPOINT}/api/usuario.php`, args)
-  if (!response.ok) throw new Error('Respuesta no OK')
-  return response.json()
+    '/api/usuario.php'
+  )
 }
 
 export async function fetchCorreos(jwt) {
-  const args = {
-    method: 'POST',
-    headers: {
-      'Content-type': 'application/json',
-    },
-    body: JSON.stringify({
+  return fetchServer(
+    JSON.stringify({
       jwt: jwt,
     }),
-  }
-  const response = await fetch(`${ENDPOINT}/api/correos.php`, args)
-  if (!response.ok) throw new Error('Respuesta no OK')
-  return response.json()
+    '/api/correos.php'
+  )
 }
 
 export async function deleteMensaje(jwt, message) {
-  const args = {
-    method: 'POST',
-    headers: {
-      'Content-type': 'application/json',
-    },
-    body: JSON.stringify({
+  return fetchServer(
+    JSON.stringify({
       jwt: jwt,
       message: message,
     }),
-  }
-  const response = await fetch(`${ENDPOINT}/api/borrarMensaje.php`, args)
-  if (!response.ok) throw new Error('Respuesta no OK')
-  return response.json()
+    '/api/borrarMensaje.php'
+  )
 }
 
 /* Cartero */
 
 export async function sendMensaje({ email, asunto, mensaje }) {
-  const args = {
-    method: 'POST',
-    headers: {
-      'Content-type': 'application/json',
-    },
-    body: JSON.stringify({
+  return fetchServer(
+    JSON.stringify({
       mail: email,
       subject: asunto,
       content: mensaje,
     }),
-  }
-  const response = await fetch(`${ENDPOINT}/api/enviarMensaje.php`, args)
-  if (!response.ok) throw new Error('Respuesta no OK')
-  return response.json()
+    '/api/enviarMensaje.php'
+  )
 }
 
 /* Admin */
 
 export async function insertUser(jwt, { correo, contrasena, imagen }) {
-  const args = {
-    method: 'POST',
-    headers: {
-      'Content-type': 'application/json',
-    },
-    body: JSON.stringify({
+  return fetchServer(
+    JSON.stringify({
       jwt: jwt,
       password: contrasena,
       user: correo,
       image: imagen,
     }),
-  }
-  const response = await fetch(`${ENDPOINT}/api/insertUser.php`, args)
-  if (!response.ok) throw new Error('Respuesta no OK')
-  return response.json()
+    '/api/insertUser.php'
+  )
 }
 
 export async function updateUser(jwt, { correo, contrasena, imagen }) {
-  const args = {
-    method: 'POST',
-    headers: {
-      'Content-type': 'application/json',
-    },
-    body: JSON.stringify({
+  return fetchServer(
+    JSON.stringify({
       jwt: jwt,
       user: correo,
       password: contrasena,
       image: imagen,
     }),
-  }
-  const response = await fetch(`${ENDPOINT}/api/updateUser.php`, args)
-  if (!response.ok) throw new Error('Respuesta no OK')
-  return response.json()
+    '/api/updateUser.php'
+  )
 }
 
 export async function deleteUsuario(jwt, mail) {
-  console.log(jwt, mail)
-  const args = {
-    method: 'POST',
-    headers: {
-      'Content-type': 'application/json',
-    },
-    body: JSON.stringify({
+  return fetchServer(
+    JSON.stringify({
       jwt: jwt,
       mail: mail,
     }),
-  }
-  const response = await fetch(`${ENDPOINT}/api/borrarUsuario.php`, args)
-  if (!response.ok) throw new Error('Respuesta no OK')
-  return response.json()
+    '/api/borrarUsuario.php'
+  )
 }
 
 /* Autentificación */
 
 export async function authUser({ user, password }) {
-  const args = {
-    method: 'POST',
-    headers: {
-      'Content-type': 'application/json',
-    },
-    body: JSON.stringify({
+  return fetchServer(
+    JSON.stringify({
       user: user,
       password: password,
     }),
-  }
-  const response = await fetch(`${ENDPOINT}/api/authUser.php`, args)
-  if (!response.ok) throw new Error('Respuesta no OK')
-  return response.json()
+    '/api/authUser.php'
+  )
 }
 
 export async function authAdmin({ user, password }) {
-  const args = {
-    method: 'POST',
-    headers: {
-      'Content-type': 'application/json',
-    },
-    body: JSON.stringify({
+  return fetchServer(
+    JSON.stringify({
       user: user,
       password: password,
     }),
-  }
-  const response = await fetch(`${ENDPOINT}/api/authAdmin.php`, args)
-  if (!response.ok) throw new Error('Respuesta no OK')
-  return response.json()
+    '/api/authAdmin.php'
+  )
 }
